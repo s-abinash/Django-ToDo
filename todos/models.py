8ca9fd54aa1defa57e3693cba8c03aa16736bdf1
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-28 18:58:21 +0530

    added table todos_todo in the database todoslist

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_at = models.DateTimeField(default=datetime.now, blank=True)
