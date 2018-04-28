f4c0468fc006ce94078d600e0c267d86b31b3a6b
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-28 19:44:36 +0530

    added view index.html for viewing todo list

--- todos/views.py
+++ todos/views.py
@@ -4,7 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Hello World')
+	todos = Todo.objects.all()[:10] #requesting 10 todo objects from Todo Model
+	context = {
+		'todos': todos
+	}
+	return render(request, 'index.html', context)
