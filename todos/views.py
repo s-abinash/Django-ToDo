c1f238979e90a8223c871cc0fd178bb25ea53708
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-28 21:52:29 +0530

    adding new todos feature enabled

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Todo
@@ -21,3 +21,14 @@ def details(request, id):
 	'todo': todo
 	}
 	return render(request, 'details.html', context)
+
+def add(request):
+	if(request.method == 'POST'):
+		title = request.POST['title']
+		text = request.POST['text']
+
+		todo = Todo(title=title, text=text)
+		todo.save()
+		return redirect('/todos')
+	else:
+		return render(request, 'add.html')
