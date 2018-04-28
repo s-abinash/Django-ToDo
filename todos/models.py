3fede4fa6ab7b36baf03dfc5ff727a0cde95e9b5
Minanshu Singh <minanshu.1998@gmail.com>
2018-04-28 19:18:06 +0530

    added def __str__(self) function to show title of todo object in admin page

--- todos/models.py
+++ todos/models.py
@@ -9,3 +9,5 @@ class Todo(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_at = models.DateTimeField(default=datetime.now, blank=True)
+	def __str__(self):
+		return self.title
